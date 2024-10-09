213faa7d0ed6a699916956ebb0bd983f3ed31612
Chris <rockhill78@gmail.com>
2024-10-09 15:52:01 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="npc-manager-ui">
+      <header>
+        <h1>NPC Manager UI</h1>
+      </header>
+      <main>
+        <p>Welcome to the NPC Manager!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
