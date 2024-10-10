e505857b2c8037dc4404b4d1a80dd279c543a7da
Chris <rockhill78@gmail.com>
2024-10-09 20:09:15 -0400

    add npc rows works

--- src/App.js
+++ src/App.js
@@ -5,34 +5,25 @@ import './index.css';
 
 const App = () => {
   const [npcs, setNpcs] = useState([
-    { name: 'Goblin', health: 30, strength: 5, agility: 10, trait:{
-        name: "goblin trait",
-        rating: 4
-    } },
-    { name: 'Orc', health: 60, strength: 8, agility: 6, trait:{
-        name: "orc trait",
-        rating: 4
-    }  },
-    { name: 'Troll', health: 90, strength: 12, agility: 4, trait:{
-        name: "troll trait",
-        rating: 4
-    }  },
-    { name: 'Elf', health: 40, strength: 6, agility: 12, trait:{
+    { name: 'Elf', trait:{
         name: "elf trait",
         rating: 4
     }  },
-    { name: 'Dwarf', health: 70, strength: 9, agility: 5, trait:{
+    { name: 'Dwarf', trait:{
         name: "dwarf trait",
         rating: 4
     }  },
-    { name: 'Dragon', health: 150, strength: 20, agility: 8, trait:{
+    { name: 'Dragon', trait:{
         name: "dragon trait",
         rating: 4
     }  },
   ]);
 
   const addNpc = () => {
-    const newNpc = { name: 'New NPC', health: 50, strength: 7, agility: 5 };
+    const newNpc = { name: 'New NPC', health: 50, strength: 7, agility: 5, trait:{
+        name: "generic trait",
+        rating: 8
+    }  };
     setNpcs([...npcs, newNpc]);
   };
 
