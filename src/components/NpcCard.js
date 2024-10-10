438b7ff67dfb5098085bb6e84a588f912e166231
Chris <rockhill78@gmail.com>
2024-10-09 23:28:32 -0400

    add npc trait wip

--- src/components/NpcCard.js
+++ src/components/NpcCard.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NpcTrait from './NpcTrait';
 
-const traits = [];
-
-let counter = 0;
-const addTrait = () => {
-  console.log('The add trait button was clicked!');
-  
-  traits.push({
-    name:"trait" + counter,
-    rating: 5
-  });
-  counter++;
-  console.log("trait array", traits);
-};
-
-
-
 const NpcCard = ({ npc }) => {
+  // Local state for traits specific to this NpcCard
+  const [traits, setTraits] = useState([]);
+  let counter = 0;
+
+  // Function to add a new trait to the traits array
+  const addTrait = () => {
+    console.log('The add trait button was clicked!');
+    const newTrait = {
+      name: "trait" + counter,
+      rating: 5
+    };
+
+    counter++;
+
+    // Update the traits state with the new trait
+    setTraits([...traits, newTrait]);
+    console.log("trait array", traits);
+    console.log("counter", counter);
+  };
+
   return (
     <div className="npc-card">
       <h2>NPC: {npc.name}</h2>
       <button onClick={addTrait}>Add New Trait</button>
-      <NpcTrait trait={npc.trait} ></NpcTrait>
+      <div>
+        {traits.map((trait, index) => (
+          <NpcTrait key={index} trait={trait} />
+        ))}
+      </div>
     </div>
   );
 };
