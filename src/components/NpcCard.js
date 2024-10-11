0de6ce3c67cf604e4793b256b02f08c2895f7afc
Chris <rockhill78@gmail.com>
2024-10-11 00:30:56 -0400

    add npc trait wip

--- src/components/NpcCard.js
+++ src/components/NpcCard.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import NpcTrait from './NpcTrait';
 
+//let counter = 0;
+
 const NpcCard = ({ npc }) => {
   // Local state for traits specific to this NpcCard
   const [traits, setTraits] = useState([]);
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
+  
 
   // Function to add a new trait to the traits array
   const addTrait = () => {
@@ -14,7 +17,7 @@ const NpcCard = ({ npc }) => {
       rating: 5
     };
 
-    counter++;
+    setCounter(counter => counter + 1);
 
     // Update the traits state with the new trait
     setTraits([...traits, newTrait]);
